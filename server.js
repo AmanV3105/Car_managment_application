d8b388818ed2ecf58c673ec2a6f1b6feecda3b3b
Aman Varshney <varshneyaman3105@gmail.com>
2024-11-17 23:11:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const dbConnection = require('./db')
-app.use(express.json())
+const express = require('express');
+const cors = require('cors');
+const dbConnection = require('./db');
 
-app.use('/api/cars/', require('./routes/carsRoute'))
-app.use('/api/users/', require('./routes/usersRoute'))
-app.use('/api/bookings/', require('./routes/bookingsRoute'))
+const app = express();
 
-app.use(cors(
-    {
-        origin: ["https://car-management-application_1.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
-const path = require('path')
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://car-management-application_1.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+app.use('/api/cars/', require('./routes/carsRoute'));
+app.use('/api/users/', require('./routes/usersRoute'));
+app.use('/api/bookings/', require('./routes/bookingsRoute'));
 
 if (process.env.NODE_ENV === 'production') {
-
-    app.use('/', express.static('client/build'))
-
-    app.get('*', (req, res) => {
-
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-
-    })
-
+  const path = require('path');
+  app.use('/', express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
 }
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-
-
-
-
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
+// No app.listen required for Vercel
+module.exports = app;
